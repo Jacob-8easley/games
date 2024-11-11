6fb54d31d8d35e4af2215115499b5190eef602d8
Jacob Beasley <beasley.jacob@outlook.com>
2024-11-11 22:00:58 +0000

    delete node modules and create food generator

--- snake/index.js
+++ snake/index.js
@@ -1,20 +1,25 @@
 //define html elements
-import jquery from "jquery";
-import express from "express";
-import ejs from "ejs";
-import pg from "pg";
-import bodyParser from "body-parser";
-import { create } from "domain";
+//import $ from "jquery";
+//import express from "express";
+//import ejs from "ejs";
+//import pg from "pg";
+//import bodyParser from "body-parser";
 
-const board = $('game-board');
+
+const board = document.getElementById('game-board')
+
+// $('#game-board)
 
 //define game variables
+const gridSize = 20;
 let snake = [{ x:10, y:10 }];
+let food = generateFood()
 
 // draw game map, snake and food
 function draw() {
     board.innerHTML = '';
     drawSnake();
+    drawFood();
 }
 
 //Draw snake
@@ -22,14 +27,14 @@ function drawSnake(){
     snake.forEach((segment) =>{
         const snakeElement = createGameElement('div','snake');
         setPosition(snakeElement, segment);
-        board.append(snakeElement);
+        board.appendChild(snakeElement);
     });
 }
 
 //create a snake or food cube 
 
 function createGameElement(tag, className){
-    const element = $(tag);
+    const element = document.createElement(tag);
     element.className = className;
     return element;
 }
@@ -38,6 +43,25 @@ function setPosition(element, position){
     element.style.gridColumn = position.x;
     element.style.gridRow = position.y;
 }
+
+// Drawing the food
+function drawFood(){
+    const foodElement = createGameElement('div','food');
+    setPosition(foodElement, food);
+    board.appendChild(foodElement)
+}
+// Generate food
+function generateFood(){
+    const x = Math.floor(Math.random()*gridSize)+1;
+    const y = Math.floor(Math.random()*gridSize)+1;
+    return {x,y};
+}
+
+
+
+
+
+
 
 //testing draw function
 
